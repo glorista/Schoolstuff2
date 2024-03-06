5808543aabdd76fd977a83c80d757b436cb113d0
glorista <gloria.greiner017@gmail.com>
2024-03-06 14:56:17 +0100

    neuer

---
+++ src/Hü2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Hü2 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Bitte geben sie ihr Alter an:");
+        int age=scanner.nextInt();
+
+        //Reihenfolge wichtig, Größte zuerst
+        if (age>=18){
+            System.out.println("Du darfst B und A machen");
+        }else if (age>=16){
+            System.out.println("Du kannst den AM machen.");
+        }else {
+            System.out.println("du bist zu jung für alles");
+        }
+
+    }
+}
---
+++ src/Hü7.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Hü7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Geben sie einen Euro Betrag an: ");
+        double euro= scanner.nextDouble();
+
+        System.out.println("Geben sie ihr neue Währung an USD,CZK,HUF oder PLN");
+        String neu= scanner.next().toUpperCase();
+
+        if (neu.equals("USD")){
+            System.out.println(euro+" sind "+usd(euro)+" Dollar");
+        } else if (neu.equals("CZK")){
+            System.out.println(euro+" sind "+czk(euro)+" CSK");
+        }else if (neu.equals("HUF")){
+            System.out.println(euro+" sind "+huf(euro)+" HUF");
+        }else if (neu.equals("PLN")){
+            System.out.println(euro+" sind "+pln(euro)+" PLN");
+        }else {
+            System.out.println("Hast du dich vertippt?");
+        }
+    }
+    public static double usd(double euro){
+        return euro*1.08;
+    }
+    public static double czk(double euro){
+        return euro*25.37;
+    }
+    public static double huf(double euro){
+        return euro*395.31;
+    }
+    public static double pln(double euro){
+        return euro*4.33;
+    }
+
+
+}
