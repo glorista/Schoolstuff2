cdecc0fc529fe87b9f71b2db9a3c4157e053a592
glorista <gloria.greiner017@gmail.com>
2024-03-19 15:54:23 +0100

    19.03

---
+++ src/Enums/Wrapperklassen.java
@@ -0,0 +1,22 @@
+package Enums;
+
+public class Wrapperklassen {
+    public static void main(String[] args) {
+        int primitiveInt= 17;
+
+        //1. Möglichkeit
+        Integer wrapperInt= primitiveInt;
+        Integer wrapperInt1= 17;
+
+        //2te Möglichkeit veraltet
+        Integer wrapperInt2= Integer.valueOf(primitiveInt);
+        Integer wrapperInt3= Integer.valueOf(17);
+
+        double primitiveDouble = 1.2;
+        Double wrappedDouble=1.2;
+
+        int pint = null;
+        Integer wint= null;
+
+    }
+}
