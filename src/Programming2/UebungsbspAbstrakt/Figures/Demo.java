e41c732cf30726301a30e24979e1134f1101e69f
glorista <gloria.greiner017@gmail.com>
2024-04-10 15:49:37 +0200

    10.04

--- src/Programming2/UebungsbspAbstrakt/Figures/Demo.java
+++ src/Programming2/UebungsbspAbstrakt/Figures/Demo.java
@@ -14,6 +14,21 @@ public class Demo {
         System.out.println("Fläche vom Dreieck: "+r.getArea());
         System.out.println("Umfang vom Dreieck: "+r.getPerimeter());
 
+        System.out.println(f.getAreaBySizeCategories());
+        System.out.println("---------");
+
+        Circle c2 = new Circle(98.45);
+        Rectangle r2  = new Rectangle(12,42);
+
+        f.add(c2);
+        f.add(r2);
+        System.out.println("Fläche vom Kreis: "+c2.getArea());
+        System.out.println("Umfang vom Kreis: "+c2.getPerimeter());
+
+        System.out.println("Fläche vom Dreieck: "+r2.getArea());
+        System.out.println("Umfang vom Dreieck: "+r2.getPerimeter());
+
+
         System.out.println(f.getAverageSize());
         System.out.println(f.getMaxPerimeter());
         System.out.println(f.getAreaBySizeCategories());
---
+++ src/Programming2/UebungsbspAbstrakt/Getraenk/Fluessigkeit.java
@@ -0,0 +1,4 @@
+package Programming2.UebungsbspAbstrakt.Getraenk;
+
+public class Fluessigkeit {
+}
