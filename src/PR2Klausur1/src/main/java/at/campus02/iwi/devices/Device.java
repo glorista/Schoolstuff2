535bbea844de8e5f96e9e5133a4d609276f13253
glorista <gloria.greiner017@gmail.com>
2024-04-23 12:29:49 +0200

    23.04

---
+++ src/PR2Klausur1/src/main/java/at/campus02/iwi/devices/Device.java
@@ -0,0 +1,37 @@
+package PR2Klausur1.src.main.java.at.campus02.iwi.devices;
+
+import PR2Klausur1.src.main.java.at.campus02.iwi.devices.Processor;
+
+public abstract class Device {
+    protected String name;
+    protected Processor processor;
+    protected int releaseYear;
+    protected int nrProduced;
+
+    public Device(String name, Processor processor, int releaseYear, int nrProduced) {
+        this.name = name;
+        this.processor = processor;
+        this.releaseYear = releaseYear;
+        this.nrProduced = nrProduced;
+    }
+
+    public void setNrProduced(int nrProduced) {
+        this.nrProduced = nrProduced;
+        if (nrProduced<0){
+            nrProduced=0;
+        }
+    }
+
+    public abstract double calculateProductionCost();
+
+    public abstract double calculateSupportCostForAYear(int year);
+
+    @Override
+    public String toString() {
+        return "Device{" +
+                "name='" + name + '\'' +
+                ", processor=" + processor +
+                ", nrProduced=" + nrProduced +
+                '}';
+    }
+}
---
+++ src/PR2Klausur1/src/main/java/at/campus02/iwi/devices/Smartphone.java
@@ -0,0 +1,49 @@
+package PR2Klausur1.src.main.java.at.campus02.iwi.devices;
+
+public class Smartphone extends Device {
+
+  protected int nrCameras;
+
+    public Smartphone(String name, Processor processor, int releaseYear, int nrProduced, int nrCameras) {
+        super(name, processor, releaseYear, nrProduced);
+        this.nrCameras = nrCameras;
+    }
+
+    @Override
+    public double calculateProductionCost() {
+        double processorKosten=processor.getGeneration()*100;
+        double kameraKosten=nrCameras*20;
+        if(nrProduced>= 100000 && nrProduced< 1000000){
+            double mengenRabatt=processorKosten+kameraKosten*0.1;
+            return mengenRabatt;
+        }else if (nrProduced>=1000000){
+            double mengenRabatt = processorKosten+kameraKosten*0.2;
+            return mengenRabatt;
+        }
+        return processorKosten+kameraKosten;
+    }
+
+    @Override
+    public double calculateSupportCostForAYear(int year) {
+        return 0;
+    }
+
+    public int getNrCameras() {
+        return nrCameras;
+    }
+
+    public void setNrCameras(int nrCameras) {
+        this.nrCameras = nrCameras;
+    }
+
+    @Override
+    public String toString() {
+        return "Smartphone{" +
+                "nrCameras=" + nrCameras +
+                ", name='" + name + '\'' +
+                ", processor=" + processor +
+                ", releaseYear=" + releaseYear +
+                ", nrProduced=" + nrProduced +
+                '}';
+    }
+}
