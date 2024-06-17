4117c802a1e6338927f73badb5b5b25df6a35de5
glorista <gloria.greiner017@gmail.com>
2024-06-17 15:55:24 +0200

    17.06

---
+++ src/UebungForUni/src/BinaerFile.java
@@ -0,0 +1,24 @@
+package UebungForUni.src;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BinaerFile {
+    public static void main(String[] args) throws IOException {
+        File f = new File("hall0.txt");
+
+        FileWriter fw = new FileWriter(f);
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        for (int i = 0; i < 20; i++) {
+            bw.write(43);
+            bw.flush();
+        }
+        bw.close();
+
+
+
+    }
+}
---
+++ src/UebungForUni/src/SimpleClient.java
@@ -0,0 +1,42 @@
+package UebungForUni.src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class SimpleClient {
+    public static void main(String[] args) {
+        String hostname = "localhost";
+        int port = 8787; // This should match the server's port
+
+        try (Socket socket = new Socket(hostname, port)) {
+            System.out.println("Connected to the server");
+
+
+            // Create input and output streams to communicate with the server
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            // Send a message to the server
+            String message = "Hello from client";
+            output.println(message);
+            while(!message.equalsIgnoreCase("q")){
+                String response = input.readLine();
+                System.out.println("Received from server: " + response);
+            }
+
+            // Read the response from the server
+
+
+            // Close the connection
+
+            socket.close();
+            System.out.println("Connection closed");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
